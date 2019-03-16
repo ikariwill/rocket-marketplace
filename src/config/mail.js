5fbd5db9716daf0c90db642f4365b6cd18194269
Willian Home <willian@wiki4fit.com.br>
2019-03-16 04:36:54 -0300

    variaveis de ambiente

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,9 +1,9 @@
 module.exports = {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: '78489550878878',
-    pass: '89c6db0fa29839'
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS
   }
 }
--- src/config/sentry.js
+++ src/config/sentry.js
@@ -1,3 +1,3 @@
 module.exports = {
-  dsn: 'https://0708daaab75e414c8af0d62e37adaff0@sentry.io/1416893'
+  dsn: process.env.SENTY_DSN
 }
